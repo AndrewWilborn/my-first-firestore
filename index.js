b7af9a3309d9417a296e686aec39121616cd2c51
Andrew Wilborn <andrewwilborn@Andrews-MacBook-Air-2.local>
2023-06-19 16:44:46 -0400

    FINISHED crud examples for firestore

--- index.js
+++ index.js
@@ -22,6 +22,12 @@ const db = getFirestore();
 //     cost: 1.5,
 //     type: 'produce'
 // }
+// const orange = {
+//     color: 'orange',
+//     cost: 2,
+//     type: 'produce'
+// }
+
 
 // Create
 // db.collection("food").add(apple).then(doc => {console.log("added item " + doc.id)}).catch(console.error);
@@ -33,3 +39,24 @@ db.collection("food").get().then(collection => {
     console.table(table);
 }).catch(console.error)
 
+// Read items with specific prices
+// .where before .get, everything else is the same
+// using await
+const collection = await db.collection("food").where('cost', '>', 1).get().catch(console.error);
+const table = collection.docs.map(doc => ({...doc.data()}));
+console.table(table);
+
+// Get single document by id
+// Use await instead of .then
+const doc = await db.collection("food").doc("BChwbIfGazGjN3jeunjt").get().catch(console.error)
+console.log(doc.data())
+
+
+// Update
+db.collection("food").doc('BChwbIfGazGjN3jeunjt').update({ color: 'yellow' })
+    .then(doc => console.log('Updated doc'))
+    .catch(console.error)
+
+
+// Delete
+// await db.collection("food").doc("BChwbIfGazGjN3jeunjt").delete().catch(console.error)
